90148b32ab914b463a92be844dda5594d6285f3b
sawdisha.Disha <sawdisha.dishasawant1672@gmail.com>
2022-03-13 10:23:02 +0530

    Updated Exercise 3 file

--- lphw/Exercise3/ex3.py
+++ lphw/Exercise3/ex3.py
@@ -1,21 +1,28 @@
+# Below line prints the statement
 print("I will now count my chickens:")
+# Below line first performs division and then addition
 print("Hens", 25 + 30 / 6)
+# Below line performs multiplication first then remainder and then substraction operaation
 print("Roosters", 100 -25 * 3 % 4)
-
+# Below line prints the statement
 print("Now I will count the eggs:")
-
+# Below line first performs division them remainder and then addition, substraction as per the sequesnce in the equation
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
-
+# Below line will print the question
 print("Is it true that 3 + 2 < 5 - 7?")
+# Below line performes oprations on the left and right side of conditional operator and then compares the result
 print(3 + 2 < 5 - 7)
-
+# Below line checks the result of left side of above equation
 print("What is 3 + 2?", 3 + 2)
+# Below line checks the result of right side of the above equation
 print("What is 5 - 7?", 5 - 7)
-
+# Below line prints the understanding of the result of comparision equation
 print("Oh, that's why it's False.")
-
+# Below line prints the statement
 print("How about some more.")
-
+# Checks if number on the left of comparision operator is greater than the number on right
 print("Is it greater?", 5 > -2)
+# Checks if the number on the left of comparision operator is greater or equal to the number on right
 print("Is it greater or equal?", 5 >= -2)
+# Checks if the number on the left of comparision operator is lesser or equal to the number on right
 print("Is it less or equal?", 5<= -2)
