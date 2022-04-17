6a117f0f468a496ef52f070eaebe400e39256787
sawdisha.Disha <sawdisha.dishasawant1672@gmail.com>
2022-04-17 11:19:23 +0530

    Exercise 8 and study drill completed

---
+++ lphw/Exercise8/ex8.py
@@ -0,0 +1,18 @@
+# Defines variable of type string
+formatter = "{} {} {} {}"
+
+# prints four numericals passed for formatter variable using format function
+print(formatter.format(1, 2, 3, 4))
+# prints four string values passed for formatter variable using format function
+print(formatter.format("one", "two", "three", "four"))
+# prints boolean values passed for formatter variable using format function
+print(formatter.format(True, False, False, True))
+# prints four variables passed for formatter variable using format function
+print(formatter.format(formatter, formatter, formatter, formatter))
+# prints four string statements passed for formatter variable using format function
+print(formatter.format(
+"Try your",
+"Own text here",
+"Maybe a poem",
+"Or a song about fear"
+))
