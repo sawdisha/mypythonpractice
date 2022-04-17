57c3ce504e8614e812f11ea464f357844a8ab625
sawdisha.Disha <sawdisha.dishasawant1672@gmail.com>
2022-04-17 19:04:00 +0530

    Exercise 9 and study drill completed

---
+++ lphw/Exercise9/ex9.py
@@ -0,0 +1,17 @@
+# Assigning string value to the variable
+days = " Mon Tue Wed Thu Fri Sat Sun"
+# Part of string after \n starts on new line
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# We are providing variable parameter to the print function
+print("Here are the days: ", days)
+# We are providing variable parameter to the print function
+print("Here are the months:", months)
+
+# We can provide multiline strings between triple quotes
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
