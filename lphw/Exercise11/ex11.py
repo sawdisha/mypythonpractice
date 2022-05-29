cf16544aab70733b33ad98ab84669c701bec8125
sawdisha.Disha <sawdisha.dishasawant1672@gmail.com>
2022-05-29 18:46:39 +0530

    Exercise 11 and Study Drill completed

---
+++ lphw/Exercise11/ex11.py
@@ -0,0 +1,24 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+# Trying few other samples
+# Get input from user
+
+inputString = input()
+print(f'The inputted string is: {inputString}')
+# Get input from user with a prompt
+inputString1 = input('Enter a string again:')
+print('The inputted string is:', inputString1)
+
+# Writing another"form"
+print("What is your name?", end='')
+name = input()
+print("Where do you stay?", end=' ')
+place = input()
+print(f'Your name is {name} and you stay in {place}.')
