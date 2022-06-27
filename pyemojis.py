cf7e3fa20634db3b6aec8e20feeb634c0a82bef1
sawdisha <dishasawant1672@gmail.com>
2022-06-27 11:27:42 +0530

    emojis Unicode

---
+++ pyemojis.py
@@ -0,0 +1,20 @@
+# grinning face
+print("\U0001f600")
+# rolling on the floor laughing
+print("\U0001f923")
+# unamused face
+print("\U0001f612")
+# zipped
+print("\U0001f910")
+# smiling face with hearts
+print("\U0001f970")
+# hug
+print("\U0001f917")
+# winking face
+print("\U0001f609")
+# smiling face with smiling eyes
+print("\U0001f60A")
+# smiling face with heart eye
+print("\U0001f60D")
+# slightly smiling face
+print("\U0001f642")
