fbc77d99b49b9ea1283e7517523b7a547272af71
sawdisha <dishasawant1672@gmail.com>
2022-08-12 14:49:55 +0530

    using filter()

---
+++ SelfLearning/filterfunc.py
@@ -0,0 +1,45 @@
+# filter() on iterable object list
+
+list = [5 , 6, 7, 23, 60, 1, 73, 34, 45, 3]
+
+def check_num(num):
+    if num > 30:
+        return True
+    else:
+        return False
+
+#filer is used to excule items in an iterable object
+greater_than_thirty = filter(check_num, list)
+
+for i in greater_than_thirty:
+    print(i)
+
+
+# filter() on iterable object dictionary
+
+dict1 = {5: "Disha", 6: "Pravin", 23:"Megha", 60:"Ahana", 1:"Bhsuhan", 73:"Raj"}
+
+# filter on key
+lesser_than_thirty = dict(filter(lambda item: item[0] <30, dict1.items()))
+print(lesser_than_thirty)
+
+
+# filter on  value
+h_present = dict(filter(lambda item: item[1].__contains__('h'), dict1.items()))
+print(h_present)
+
+
+# filter() on iterable object string
+
+str = "Hello"
+
+def check_char(char):
+    if char == 'l':
+        return True
+    else:
+        return False
+
+check_l = filter(check_char, str)
+
+for k in check_l:
+    print(k)
